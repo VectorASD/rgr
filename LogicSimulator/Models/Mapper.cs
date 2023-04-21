8c6db2f3948d0564531aa62eddd61af1e02d4b0d
VectorASD <60642904+VectorASD@users.noreply.github.com>
2023-04-21 20:57:07 +0700

    Теперь элементы можно перемещать

--- LogicSimulator/Models/Mapper.cs
+++ LogicSimulator/Models/Mapper.cs
@@ -1,31 +1,107 @@
 ﻿using Avalonia.Controls;
 using Avalonia;
 using LogicSimulator.ViewModels;
+using LogicSimulator.Views.Shapes;
+using System;
 
 namespace LogicSimulator.Models {
     public class Mapper {
+        /*
+         * Определение режима перемещения
+         */
+
+        int mode = 0;
+        /*
+         *    Режимы:
+         * 0 - ничего не делает
+         * 1 - двигаем камеру
+         * 2 - двигаем элемент
+        */
+
+        private void CalcMode(Control item) {
+            var c = (string?) item.Tag;
+            mode = c switch {
+                "Scene" => 1,
+                "Body" => 2,
+                "Pin" or _ => 0,
+            };
+        }
+
+        private UserControl? GetUC(Control item) {
+            while (item.Parent != null) {
+                if (item is UserControl @UC) return @UC;
+                item = (Control) item.Parent;
+            }
+            return null;
+        }
+        private IGate? GetGate(Control item) {
+            var UC = GetUC(item);
+            if (UC is IGate @gate) return @gate;
+            return null;
+        }
 
         /*
          * Обработка мыши
          */
 
+        Point moved_pos;
+        IGate? moved_item;
+        bool tapped = false;
+        Point item_old_pos;
+
+        public Point tap_pos;
+
         public void Press(Control item, Point pos) {
-            Log.Write("PointerPressed: " + item.GetType().Name + " pos: " + pos);
+            // Log.Write("PointerPressed: " + item.GetType().Name + " pos: " + pos);
+
+            CalcMode(item);
+            Log.Write("new_mode: " + mode);
+
+            moved_pos = pos;
+            moved_item = GetGate(item);
+            tapped = true;
+            if (moved_item != null) item_old_pos = moved_item.GetPos();
+
+            switch (mode) {
+            case 2:
+                break;
+            }
 
             Move(item, pos);
         }
 
         public void Move(Control item, Point pos) {
-            Log.Write("PointerMoved: " + item.GetType().Name + " pos: " + pos);
+            // Log.Write("PointerMoved: " + item.GetType().Name + " pos: " + pos);
+
+            var delta = pos - moved_pos;
+            if (delta.X == 0 && delta.Y == 0) return;
+
+            if (Math.Pow(delta.X, 2) + Math.Pow(delta.Y, 2) > 9) tapped = false;
+
+            switch (mode) {
+            case 2:
+                if (moved_item == null) break;
+                var new_pos = item_old_pos + delta;
+                moved_item.Move(new_pos);
+                break;
+            }
         }
 
         public void Release(Control item, Point pos) {
             Move(item, pos);
-            Log.Write("PointerReleased: " + item.GetType().Name + " pos: " + pos);
+            // Log.Write("PointerReleased: " + item.GetType().Name + " pos: " + pos);
+
+            if (tapped) Tapped(item, pos);
+            mode = 0;
+        }
+
+        private void Tapped(Control item, Point pos) {
+            Log.Write("Tapped: " + item.GetType().Name + " pos: " + pos);
+            tap_pos = pos;
         }
 
         public void WheelMove(Control item, double move) {
-            Log.Write("WheelMoved: " + item.GetType().Name + " delta: " + (move > 0 ? 1 : -1));
+            // Log.Write("WheelMoved: " + item.GetType().Name + " delta: " + (move > 0 ? 1 : -1));
         }
     }
 }
