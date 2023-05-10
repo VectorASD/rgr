5ba2d39ec371aea9d0072165319cb52db884e6fd
Прилепа Михаил <vectorasd@vk.com>
2023-05-10 22:16:56 +0700

    Реализовал зачатки экстрактора ER-диаграмм

---
+++ ER-diagram_exTRACTOR/Program.cs
@@ -0,0 +1,22 @@
+﻿using LogicSimulator.Models;
+using System.Reflection;
+
+// LogicSimulator.Program.Main(Array.Empty<string>()); Ура! Консольный режим изобретён ;'-}
+// Type[] types = Assembly.GetExecutingAssembly().GetTypes();
+// Type[] types = new Type[] { typeof(Mapper) };
+
+// Вот это по нашему:
+Type[] types = (Assembly.GetAssembly(typeof(Mapper)) ?? throw new Exception("Чё?!")).GetTypes();
+
+foreach (Type type in types) {
+    var name = type.FullName ?? throw new Exception("Чё?!");
+    if (name.Contains('+') || !name.StartsWith("LogicSimulator.")) continue;
+
+    Console.WriteLine("\nT: " + name);
+    
+    foreach (var mem in type.GetMembers()) {
+        if (mem.Module.Name != "LogicSimulator.dll") continue;
+
+        Console.WriteLine($"  {mem.Name, -36} | {mem.MemberType, -12} | {mem.DeclaringType == type}");
+    }
+}
